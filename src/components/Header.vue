d2e91c2732a2f2cd67a10c7768a6cd8dbf81b0fb
muniz <muniz@daniel.muniz>
2025-03-01 16:57:32 -0300

    removing navbar toggler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,6 @@
   <header class="navbar">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Sports API</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Início</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sobre</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contato</a>
-          </li>
-        </ul>
-      </div>
     </div>
   </header>
 </template>
