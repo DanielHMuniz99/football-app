2a20fd381b9161321230874b59b19bd32aba2d67
muniz <muniz@daniel.muniz>
2025-03-01 16:55:09 -0300

    import bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-
 import router from './router';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 const app = createApp(App);
 app.use(router);
